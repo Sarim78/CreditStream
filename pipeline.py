6b4970c01dd8b357ea67c60779c955783aa929e1
Sarim <74938278+Sarim78@users.noreply.github.com>
2026-03-31 21:02:53 -0400

    Setting Up the Environment

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,23 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import os
+
+# Setting Up the Environment
+Input_FILE = "data/loan_data.csv"
+output_DIR = "output"
+Database_Name = "creditstream.db"
+Table_Name = "loans"
+
+def extract_data(filepath):
+    pass 
+
+def transform(df):
+    pass
+
+def load(df, output_dir, database_name, table_name):
+    pass
+
+def summary(df):
+    pass
+
+# Run The Pipeline
